e6de48a83ed9b9721329edafb25a95494fac0c03
Muhammad Ashraf <mohamedashraf8850@gmail.com>
2018-11-17 21:11:20 +0200

    Java Files
    Main - Splash - Login - Signup

--- app/src/main/java/com/example/youssef/mamafelshare3/MainActivity.java
+++ app/src/main/java/com/example/youssef/mamafelshare3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.youssef.mamafelshare3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* this  Code By :  Muhammad Ashraf
+        ( while Open SplashScreen This code will be able to show the (SplashLoginScreen) for Only First Show this App )
+
+        ( I wil disable it -- when us Publish this App make it showen)
+         */
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            startActivity(new Intent(this, SplashLoginScreen.class));
+        }
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).commit();
     }
 }
